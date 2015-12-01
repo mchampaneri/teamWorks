327205863233ec168ddb525576658f831f54ceca
Manish Champaneri <m.champaneri.20@gmail.com>
2015-12-01 19:54:45 +0530

    Fianl Initial setup

---
+++ app/database/migrations/2015_12_01_140559_create_project_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('project_name');
+			$table->string('description');
+			$table->string('client');
+			$table->integer('coordinator-id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
